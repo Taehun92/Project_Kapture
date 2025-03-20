7ed81a70d22c2d57343bb2ea8bf43b709e4b70eb
codingjs12 <202245068@itc.ac.kr>
2025-03-20 19:46:55 +0900

    2025.03.20 board 내부 코드 삭제

---
+++ kapture/src/main/java/com/example/kapture/admin/controller/AdminController.java
@@ -0,0 +1,10 @@
+package com.example.kapture.admin.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AdminController {
+
+	
+	
+}
--- kapture/src/main/java/com/example/kapture/board/model/Board.java
+++ kapture/src/main/java/com/example/kapture/board/model/Board.java
@@ -4,19 +4,5 @@ import lombok.Data;
 
 @Data
 public class Board { 
-	//기본적으로 카멜 표기법, "_" 언더바가 있을 경우 언더바 생략 후 카멜표기법으로!!
-	private String boardNo;
-	private String title;
-	private String contents;
-	private String userId;
-	private String kind;
-	private String favorite;
-	private String cnt;
-	private String subtitle;
-	private String deleteYn;
-	private String cdateTime;
-	private String udateTime;
-	private String test;
-	
 	
 }
