9f370fdd0a4d8c626ce158746b465224f427b85a
geon-hyung <ooo17o@naver.com>
2025-03-20 17:38:15 +0900

    2025 03 20 team

--- kapture/src/main/java/com/example/kapture/login/dao/LoginService.java
+++ kapture/src/main/java/com/example/kapture/login/dao/LoginService.java
@@ -3,7 +3,9 @@ package com.example.kapture.login.dao;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import com.example.kapture.login.mapper.LoginMapper;
 import com.example.kapture.login.model.Login;
 
@@ -18,34 +20,48 @@ public class LoginService {
 	@Autowired
 	HttpSession session;
 	
+	@Autowired
+	PasswordEncoder passwordEncoder;
 	
-//	@Autowired
-//	PasswordEncoder passwordEncoder;
+	
+	//로그인 
+	public HashMap<String, Object> userLogin(HashMap<String, Object> map) {
+			// TODO Auto-generated method stub
+			HashMap<String, Object> resultMap = new HashMap<String, Object>();
+			Login login = loginMapper.getlogin(map);
+			boolean loginFlg = false;
+			if(login != null) {
+					loginFlg = passwordEncoder.matches((String) map.get("password"), login.getPassword());
+					
+				}
+				
+				if(loginFlg) {
+					System.out.println("성공");
+					session.setAttribute("sessionId", login.getUserNo());
+					session.setAttribute("sessionName", login.getUserFirstName());
+					session.setAttribute("sessionName", login.getUserLastName());
+					session.setAttribute("sessionStatus", login.getRole());
+					
+					resultMap.put("login", login);
+					resultMap.put("result", "success");
+				} else {
+					System.out.println("실패");
+					resultMap.put("result", "fail");
+				}
+				
+				return resultMap;
+			}
+//회원가입 
+	public HashMap<String, Object> addUser(HashMap<String, Object> map) {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		String hashPwd = passwordEncoder.encode((String) map.get("password"));
+		map.put("password", hashPwd);
+		
+		int num = loginMapper.insertUser(map);
+		resultMap.put("result", "success");
+		// if num > 0 데이터 삽입 잘 된거 
+		// 아니면 뭔가 문제 있는거
+		return resultMap;
+	}
 
-//	public HashMap<String, Object> userLogin(HashMap<String, Object> map) {
-//			// TODO Auto-generated method stub
-//			HashMap<String, Object> resultMap = new HashMap<String, Object>();
-//			Login login = loginMapper.getlogin(map);
-//			boolean loginFlg = false;
-//			if(login != null) {
-////				loginFlg = passwordEncoder.matches((String) map.get("password"), login.getPassword());
-//				
-//			}
-//			
-//			if(loginFlg) {
-//				System.out.println("성공");
-//				session.setAttribute("sessionId", login.getUserNo());
-//				session.setAttribute("sessionName", login.getUserFirstName());
-//				session.setAttribute("sessionName", login.getUserLastName());
-//				session.setAttribute("sessionStatus", login.getRole());
-//				
-//				resultMap.put("login", login);
-//				resultMap.put("result", "success");
-//			} else {
-//				System.out.println("실패");
-//				resultMap.put("result", "fail");
-//			}
-//			
-//			return resultMap;
-//		}
 }
