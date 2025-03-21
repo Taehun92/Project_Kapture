3436c220310bb6d6694d6819a3f15ac55bf40007
Taehun92 <taehun1218@gmail.com>
2025-03-21 15:22:38 +0900

    2025.03.21 로그아웃 완료 / 번역 작업 중

--- kapture/src/main/java/com/example/kapture/login/controller/LoginController.java
+++ kapture/src/main/java/com/example/kapture/login/controller/LoginController.java
@@ -55,13 +55,21 @@ public class LoginController {
 	
 	
 	
-//	 로그인
+		// 로그인
 		@RequestMapping(value = "/login.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 		@ResponseBody
 		public String login(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
 			HashMap<String, Object> resultMap = new HashMap<String, Object>();
-			
 			resultMap = loginService.userLogin(map);
+			return new Gson().toJson(resultMap);
+		}
+		
+		// 로그아웃
+		@RequestMapping(value = "/logout.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+		@ResponseBody
+		public String logout(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
+			HashMap<String, Object> resultMap = new HashMap<String, Object>();
+			resultMap = loginService.userLogout(map);
 			return new Gson().toJson(resultMap);
 		}
 		
